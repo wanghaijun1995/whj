4febb5c87bdb6bbfd94186aa20a7ddd4fa9e0086
wanghaijun1995 <673473929@qq.com>
2020-06-17 15:11:16 +0800

    Initial commit

---
+++ src/main/java/com/wang/whj/WhjApplication.java
@@ -0,0 +1,13 @@
+package com.wang.whj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wang/whj/WhjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.whj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
